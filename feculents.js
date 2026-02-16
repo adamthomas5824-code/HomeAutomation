67100248ff6875cbf30d480d3665dc742cfa51da
adamthomas5824-code <adam.thomas.5824@gmail.com>
2026-02-16 19:13:34 +0100

    Create feculents.js

---
+++ feculents.js
@@ -0,0 +1,5 @@
+const listFeculents = [
+    "Riz Basmati", "Pâtes Pennes", "Écrasé de pommes de terre", "Pâtes fraîches",
+    "Pommes de terre grenailles", "Quinoa aux herbes", "Semoule fine", "Polenta crémeuse",
+    "Gnocchis poêlés", "Lentilles vertes", "Patate douce rôtie", "Blé (Ebly)"
+];
