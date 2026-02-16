8afc485ce12c94c7f5344ea3252d7e55c5f6abd3
adamthomas5824-code <adam.thomas.5824@gmail.com>
2026-02-16 19:13:52 +0100

    Create legumes.js

---
+++ legumes.js
@@ -0,0 +1,6 @@
+const listLegumes = [
+    "Carottes vapeur", "Courgettes rôties", "Haricots verts", "Brocolis vapeur", 
+    "Fondue de poireaux", "Épinards frais à l'ail", "Poivrons au wok", "Aubergines au four", 
+    "Ratatouille (Bocal)", "Chou-fleur gratiné", "Pois gourmands", "Fenouil braisé",
+    "Purée de céleri-rave", "Salade verte croquante"
+];
