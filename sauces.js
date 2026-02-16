f1294c0ab7fabf45eee994b551958f6995736324
adamthomas5824-code <adam.thomas.5824@gmail.com>
2026-02-16 19:13:14 +0100

    Create sauces.js

---
+++ sauces.js
@@ -0,0 +1,6 @@
+const listSauces = [
+    "Curry & Lait de coco", "Moutarde à l'ancienne & Miel", "Tomate & Basilic", 
+    "Crème & Champignons", "Teriyaki (Soja/Miel)", "Citron & Huile d'olive", 
+    "Béarnaise (Thermomix)", "Hollandaise", "Pesto maison", "Béchamel légère",
+    "Vinaigrette échalote", "Sauce Aurore", "Satay (Cacahuète)"
+];
