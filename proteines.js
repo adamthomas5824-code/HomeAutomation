2640791ff5b6a2745db0d8656cd4f8da06d21cf3
adamthomas5824-code <adam.thomas.5824@gmail.com>
2026-02-16 19:12:57 +0100

    Create proteines.js

---
+++ proteines.js
@@ -0,0 +1,8 @@
+const listProteines = [
+    "Poulet : Blancs vapeur farcis", "Poulet : Émincé minute au wok", "Poulet : Cuisses rôties", 
+    "Poulet : Aiguillettes panées", "Bœuf : Steak / Entrecôte grillée", "Bœuf : Émincé façon Thai",
+    "Bœuf : Boulettes au four", "Bœuf : Rôti saignant", "Veau : Escalope fine", 
+    "Veau : Paupiettes braisées", "Veau : Émincé Blanquette", "Cheval : Steak saisi", 
+    "Poisson : Dos de cabillaud vapeur", "Poisson : Pavé de saumon grillé", "Saucisses de poulet Air-fryer",
+    "Jambon : Tranches épaisses", "Œufs : Omelette baveuse", "Lentilles Corail", "Lapin à la moutarde"
+];
